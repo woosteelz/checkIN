6b6a6d1f2183823a18aa85b276343d80b884997c
woosteelz <woosteelz@naver.com>
2020-03-22 22:39:58 +0900

    app.vue Change

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <v-app>
-
     <!-- 네비게이션 드로어 영역-->
     <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
+      <v-list nav dense class="py-0">
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>USERNAME</v-list-item-title>
+            <v-list-item-subtitle>userEmail</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
         <v-list-item-group
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
-          <v-list-item router :to="{ name: 'Home' }">
+          <v-list-item router :to="{ name: 'Home' }" exact>
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Home</v-list-item-title>
           </v-list-item>
 
-          <v-list-item router :to="{ name: 'SignIn' }">
+          <v-list-item router :to="{ name: 'SignIn' }" exact>
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
@@ -26,10 +38,86 @@
     </v-navigation-drawer>
 
     <!-- app 바 영역 -->
-    <v-app-bar height="50" color="deep-purple" dark dense>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    <v-app-bar max-height="48" dark dense>
+      <v-app-bar-nav-icon @click="drawer = true" />
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title style="width:300px">
+        <span>Title</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        rounded
+        dense
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      />
+      <v-spacer />
+
+      <v-btn icon tile color="red">
+        <v-icon color="white">mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      
+        <v-menu offset-y bottom left close-on-click>
+          <template v-slot:activator="{ on }">
+            <v-btn tile dark icon v-on="on">
+              <v-icon large="" color="blue lighten-2"
+                >mdi-account-circle</v-icon
+              >
+            </v-btn>
+          </template>
+        
+          <v-list-item two-line>
+            <v-list-item-avatar>
+              <img src="" />
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title><span class="mx-1">USERNAME</span></v-list-item-title>
+              <v-list-item-subtitle>
+                <v-icon small class="my-2">mdi-settings-outline</v-icon>
+                <span class="ma-1">프로필 편집</span>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item router :to="{ name: 'Home' }" exact>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item router :to="{ name: 'SignIn' }" exact>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Account</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item @click="signOut">
+              <v-list-item-icon>
+                <v-icon>mdi-logout-variant</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Signout</v-list-item-title>
+            </v-list-item>
+
+          </v-list-item-group>
+        </v-menu>
+      
     </v-app-bar>
 
     <!-- 컨텐츠 탈부착 영역 -->
@@ -42,10 +130,21 @@
 </template>
 
 <script>
+import { mapActions} from 'vuex'
+
 export default {
   data: () => ({
-    group: '',
-    drawer: false
-  })
+    group: "",
+    drawer: false,
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" }
+    ]
+  }),
+  methods: {
+    ...mapActions(['signOut'])
+  }
 };
 </script>
---
+++ src/components/AddSite.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      새 사이트 등록
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        ref="name"
+        v-model="name"
+        :rules="[() => !!name || 'This field is required']"
+        :error-messages="errorMessages"
+        label="사용자 지정 이름"
+        placeholder="사이트의 이름을 입력해주세요."
+        required
+      />
+      <v-text-field
+        ref="URL"
+        v-model="URL"
+        :rules="[
+          () => !!address || 'This field is required',
+          () => !!address && address.length <= 25 || 'Address must be less than 25 characters',
+          addressCheck
+        ]"
+        label="URL"
+        placeholder="사이트의 로그인 양식이 있는 URL을 입력해주세요."
+        counter="25"
+        required
+      />
+      <v-text-field
+        ref="ID"
+        v-model="ID"
+        :rules="[() => !!city || 'This field is required', addressCheck]"
+        label="아이디"
+        placeholder="해당 사이트에 로그인하기 위한 아이디를 입력해주세요."
+        required
+      />
+      <v-text-field
+        ref="password"
+        v-model="password"
+        :rules="[() => !!state || 'This field is required']"
+        label="State/Province/Region"
+        required
+        placeholder="해당 사이트에 로그인하기 위한 비밀번호를 입력해주세요."
+      />
+      <v-divider class="mt-12"></v-divider>
+      <v-card-actions>
+          <v-btn text>취소</v-btn>
+          <v-spacer></v-spacer>
+          <v-slide-x-reverse-transition>
+            <v-tooltip
+              v-if="formHasErrors"
+              left
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  class="my-0"
+                  @click="resetForm"
+                  v-on="on"
+                >
+                  <v-icon>mdi-refresh</v-icon>
+                </v-btn>
+              </template>
+              <span>Refresh form</span>
+            </v-tooltip>
+          </v-slide-x-reverse-transition>
+          <v-btn color="primary" text @click="submit">등록</v-btn>
+        </v-card-actions>
+  </v-card-text>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      URL: '',
+      ID: '',
+      password: ''
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
