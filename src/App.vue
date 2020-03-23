e5ea3fed3ede2af2f635075ccff9887fdc308f8f
ParadiseCHOI <cnw950@gmail.com>
2020-03-23 17:00:42 +0900

    [20.03.23] [App.vue]updating, [Edit.vue]creating

--- src/App.vue
+++ src/App.vue
@@ -66,64 +66,9 @@
         <v-icon color="white">mdi-heart</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-      
+      <edit/>
       <profile/>
-      <!-- <v-menu offset-y bottom left close-on-click>
-        <template v-slot:activator="{ on }">
-          <v-btn tile dark icon v-on="on">
-            <v-icon large="" color="blue lighten-2">mdi-account-circle </v-icon>
-          </v-btn>
-        </template>
 
-        <v-list-item two-line>
-          <v-list-item-avatar>
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-
-            <v-list-item-title>
-              <span class="mx-1">USERNAME</span>
-            </v-list-item-title>
-            
-            <v-list-item-subtitle>
-              <v-icon small class="my-2">mdi-settings-outline</v-icon>
-              <span class="ma-1">프로필 편집</span>
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item router :to="{ name: 'Home' }" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item router :to="{ name: 'SignIn' }" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item @click="signOut">
-            <v-list-item-icon>
-              <v-icon>mdi-logout-variant</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Signout</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-menu> -->
     </v-app-bar>
 
     <!-- 컨텐츠 탈부착 영역 -->
@@ -164,10 +109,12 @@
 <script>
 import { mapActions } from "vuex";
 import Profile from "@/components/Profile"
+import Edit from "@/components/Edit"
 
 export default {
   components: {
-    "profile": Profile
+    "profile": Profile,
+    "edit": Edit
   },
   data: () => ({
     group: "",
---
+++ src/components/Edit.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn v-if="isSignedIn" tile icon>
+    <v-icon large color="white">mdi-pencil</v-icon>
+  </v-btn>
+  <v-btn v-else tile icon disabled>
+    <v-icon large color="white">mdi-pencil</v-icon>
+  </v-btn>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState(["isSignedIn"])
+  }
+}
+</script>
+
+<style>
+
+</style>
