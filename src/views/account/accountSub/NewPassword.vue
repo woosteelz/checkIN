a68ce9b3f2ed78ed25d9a8a7fd7acf9a58ad21a5
woosteelz <woosteelz@naver.com>
2020-04-30 14:52:12 +0900

    [account/MyAccount.vue, accountSub/ChangePassword.vue] Updating, [accountSub/NewPassword.vue] Creating

--- src/views/account/accountSub/NewPassword.vue
+++ src/views/account/accountSub/NewPassword.vue
@@ -5,27 +5,27 @@
         <span class="white--text"><h3>새 비밀번호 입력</h3></span>
         <br />
         <ValidationObserver>
-        <form>
-        <VTextFieldWithValidation
-          vid="agentPW"
-          color="blue"
-          rules="required|password|min:6"
-          v-model="agentPW"
-          label="Password"
-          type="password"
-          placeholder="Please enter your Password"
-        />
-        <VTextFieldWithValidation
-          color="blue"
-          rules="required|confirmed:agentPW"
-          v-model="confirmPassword"
-          label="Password Confirmation"
-          type="password"
-          placeholder="Please enter your Confirm Password"
-        />
-        </form>
+          <form>
+            <VTextFieldWithValidation
+              vid="agentPW"
+              color="blue"
+              rules="required|password|min:6"
+              v-model="agentPW"
+              label="Password"
+              type="password"
+              placeholder="Please enter your Password"
+            />
+            <VTextFieldWithValidation
+              color="blue"
+              rules="required|confirmed:agentPW"
+              v-model="confirmPassword"
+              label="Password Confirmation"
+              type="password"
+              placeholder="Please enter your Confirm Password"
+            />
+          </form>
         </ValidationObserver>
-        <br/>
+        <br />
         <v-spacer />
         <div class="d-flex justify-end">
           <v-btn small>submit</v-btn>
